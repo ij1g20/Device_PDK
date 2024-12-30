7da15a9de866ac521504182d5ce961d661cadf78
ij1g20 <ij1g20@soton.ac.uk>
2024-12-30 15:28:55 +0000

    Add files via upload

---
+++ Nazca/grating_couplers/LPCVD_CBand_TM_grating_coupler.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 14:47:15 2024
+
+@author: isaac
+"""
+import nazca as nd 
+import nazca.mask_elements as ele
+
+def grating_coupler_template():
+    #Grating coupler template based on Tam's 477_PlasmoniAC_Passive GDS
+    
+    #Parameters
+    cell_name="LPCVD C Band TM Grating Coupler"
+    waveguide_width=1;
+    grating_width=14;
+    taper_length=300
+    pre_grating_length=10;
+    post_grating_length=10;
+    period=1.396;
+    fill_factor=0.56;
+    number_of_gratings=70;
+    
+    #GDS creation code
+    with nd.Cell(name=cell_name) as GC:
+        ele.strt(pre_grating_length, grating_width).put()
+        grating_on_width=period*fill_factor;
+        grating_off_width=period-grating_on_width;
+        number_of_gratings-=1;
+        for i in range(number_of_gratings):
+            ele.strt(grating_on_width, grating_width).put(pre_grating_length+grating_on_width+i*period)
+        ele.strt(post_grating_length, grating_width).put(pre_grating_length+grating_off_width+number_of_gratings*period);
+        taper=ele.taper(taper_length, grating_width, waveguide_width).put();
+        nd.Pin('b0',pin=nd.cp.here()).put()
+        nd.put_stub();
+    return GC
