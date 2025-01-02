cc547adfc5d38d96c619c8e16a8e2d2766d96a10
ij1g20 <ij1g20@soton.ac.uk>
2025-01-02 16:03:06 +0000

    Add files via upload

---
+++ Nazca/MMIs/MMI_template.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 12:43:37 2025
+
+@author: ij1g20
+"""
+
+import nazca as nd
+import nazca.mask_elements as ele
+
+
+def MMI_template(cell_name, number_of_inputs, number_of_outputs, taper_length, multimode_length, multimode_width, taper_width, taper_period, waveguide_width):
+       with nd.Cell(name=cell_name) as MMI:
+            # input tapers
+            for i in range(0, number_of_inputs):  # odd number of tapers
+                taper = ele.taper(taper_length, waveguide_width, taper_width).put(0, (number_of_inputs-1-i*2)*taper_period/2, 0)
+                nd.Pin('a' + str(i), pin=taper.pin['a0']).put()
+
+            #multimode section
+            ele.strt(multimode_length, multimode_width).put(taper_length+multimode_length,0,180)
+            
+            #output tapers
+            for i in range(0, number_of_outputs): #odd number of tapers
+                taper=ele.taper(taper_length,taper_width,waveguide_width).put(taper_length+multimode_length, (number_of_outputs-1-i*2)*taper_period/2, 0)
+                nd.Pin('b' +str(i),pin=taper.pin['b0']).put()
+            return MMI
---
+++ Nazca/MMIs/MMI_template_example.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 15:11:09 2025
+
+@author: ij1g20
+"""
+
+import nazca as nd
+import MMI_template as MMI
+import nazca.mask_elements as ele
+
+#Grating Coupler Parameters
+cell_name="7x8MMI example";
+number_of_inputs=7;
+number_of_outputs=8;
+taper_length=40;
+multimode_length=100;
+multimode_width=40;
+taper_width=3.4;
+taper_period=4.45;
+waveguide_width=1;
+
+mmi=MMI.MMI_template(cell_name, number_of_inputs, number_of_outputs, taper_length, multimode_length, multimode_width, taper_width, taper_period, waveguide_width).put();
+
+WG=ele.strt(10,1);
+
+WG.put(mmi.pin['a0']);
+WG.put(mmi.pin['a1']);
+WG.put(mmi.pin['a2']);
+WG.put(mmi.pin['a3']);
+WG.put(mmi.pin['a4']);
+WG.put(mmi.pin['a5']);
+WG.put(mmi.pin['a6']);
+
+WG.put(mmi.pin['b0']);
+WG.put(mmi.pin['b1']);
+WG.put(mmi.pin['b2']);
+WG.put(mmi.pin['b3']);
+WG.put(mmi.pin['b4']);
+WG.put(mmi.pin['b5']);
+WG.put(mmi.pin['b6']);
+WG.put(mmi.pin['b7']);
+
+
+
+nd.export_gds(filename='MMI template example.gds')
