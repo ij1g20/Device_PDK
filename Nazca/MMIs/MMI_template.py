9faab881fb7677296cece8748217c9eb2c3fc532
ij1g20 <ij1g20@soton.ac.uk>
2025-01-06 14:42:46 +0000

    Update MMI_template.py

--- Nazca/MMIs/MMI_template.py
+++ Nazca/MMIs/MMI_template.py
@@ -9,11 +9,11 @@ import nazca as nd
 import nazca.mask_elements as ele
 
 
-def MMI_template(cell_name, number_of_inputs, number_of_outputs, taper_length, multimode_length, multimode_width, taper_width, taper_period, waveguide_width):
+def MMI_template(cell_name, number_of_inputs, number_of_outputs, taper_length, multimode_length, multimode_width, taper_width, taper_separation, waveguide_width):
        with nd.Cell(name=cell_name) as MMI:
             # input tapers
             for i in range(0, number_of_inputs):  # odd number of tapers
-                taper = ele.taper(taper_length, waveguide_width, taper_width).put(0, (number_of_inputs-1-i*2)*taper_period/2, 0)
+                taper = ele.taper(taper_length, waveguide_width, taper_width).put(0, (number_of_inputs-1-i*2)*(taper_width+taper_separation)/2, 0)
                 nd.Pin('a' + str(i), pin=taper.pin['a0']).put()
 
             #multimode section
@@ -21,6 +21,6 @@ def MMI_template(cell_name, number_of_inputs, number_of_outputs, taper_length, m
             
             #output tapers
             for i in range(0, number_of_outputs): #odd number of tapers
-                taper=ele.taper(taper_length,taper_width,waveguide_width).put(taper_length+multimode_length, (number_of_outputs-1-i*2)*taper_period/2, 0)
+                taper=ele.taper(taper_length,taper_width,waveguide_width).put(taper_length+multimode_length, (number_of_outputs-1-i*2)*(taper_width+taper_separation)/2, 0)
                 nd.Pin('b' +str(i),pin=taper.pin['b0']).put()
             return MMI
