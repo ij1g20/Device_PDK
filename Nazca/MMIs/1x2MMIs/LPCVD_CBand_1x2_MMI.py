34e0ad5c6fa3c4831630313b8a8a48823a414864
ij1g20 <ij1g20@soton.ac.uk>
2025-01-06 14:41:42 +0000

    Update LPCVD_CBand_1x2_MMI.py

--- Nazca/MMIs/1x2MMIs/LPCVD_CBand_1x2_MMI.py
+++ Nazca/MMIs/1x2MMIs/LPCVD_CBand_1x2_MMI.py
@@ -17,12 +17,12 @@ def LPCVD_CBand_1x2_MMI():
     multimode_length=36.8;
     multimode_width=7.874;
     taper_width=3.4;
-    taper_period=3.4+1.05;
+    taper_separation=1.05;
     waveguide_width=1;   
     with nd.Cell(name=cell_name) as MMI:
             # input tapers
             for i in range(0, number_of_inputs):  # odd number of tapers
-                taper = ele.taper(taper_length, waveguide_width, taper_width).put(0, (number_of_inputs-1-i*2)*taper_period/2, 0)
+                taper = ele.taper(taper_length, waveguide_width, taper_width).put(0, (number_of_inputs-1-i*2)*(taper_width+taper_separation)/2, 0)
                 nd.Pin('a' + str(i), pin=taper.pin['a0']).put()
 
             #multimode section
@@ -30,6 +30,6 @@ def LPCVD_CBand_1x2_MMI():
             
             #output tapers
             for i in range(0, number_of_outputs): #odd number of tapers
-                taper=ele.taper(taper_length,taper_width,waveguide_width).put(taper_length+multimode_length, (number_of_outputs-1-i*2)*taper_period/2, 0)
+                taper=ele.taper(taper_length,taper_width,waveguide_width).put(taper_length+multimode_length, (number_of_outputs-1-i*2)*(taper_width+taper_separation)/2, 0)
                 nd.Pin('b' +str(i),pin=taper.pin['b0']).put()
             return MMI
